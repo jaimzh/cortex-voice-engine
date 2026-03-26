89fab0391327947b38bdc68bc8a0bc9a9271475e
jaimzh <jameshenshaw10@gmail.com>
2026-03-26 21:34:46 +0100

    fix: bat script update

--- run_cortex.bat
+++ run_cortex.bat
@@ -1,12 +1,12 @@
 @echo off
 REM Activate the virtual environment
-call venv\Scripts\activate
+call .\venv\Scripts\activate
 
 REM Printing out a starter message
 echo Setting up voice to text
 
 REM Run the main script
-python main.py
+fo
 
 REM Keep the window open if the script finishes or crashes
 
